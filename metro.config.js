574365a98c2e4bddcff1f158bb5a346baa7bd570
Expo Animation Developer <dev@example.com>
2025-04-12 17:18:11 -0400

    Simplify metro.config.js to fix module not found error

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,5 @@
 const { getDefaultConfig } = require('expo/metro-config');
-const { withMetroConfig } = require('react-native-reanimated/metro');
 
 const config = getDefaultConfig(__dirname);
 
-module.exports = withMetroConfig(config);
+module.exports = config;
